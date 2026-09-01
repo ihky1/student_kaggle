ab48c3ffd8180426573c92357cd9644bb06efc72
Hlib Kyba <hlib.kyba@student.tuke.sk>
2026-09-01 09:21:15 +0200

    stylistic changes

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,15 +1,8 @@
 import pandas as pd
 
-
 df = pd.read_csv("../data/raw/data.csv")
 
 df['grade_change'] = round(df['final_exam_score'] - df['previous_grade'], 3)
 df['grade_improved'] = df['grade_change'] > 0
-
-# SOME EXPECTATIONS
-
-
-
-
 
 df.to_csv("../data/processed/data.csv", index=False)
